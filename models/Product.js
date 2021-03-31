4d55dba3cf7ed4d15d4b916bfe17642b3b77d01d
Dom Rafiti <dom.rafiti@gmail.com>
2021-03-30 20:41:48 -0600

    adding updated requirements and inital pass on category and product models

--- models/Product.js
+++ models/Product.js
@@ -12,47 +12,57 @@ Product.init(
     // define columns
     /*
         * `Product`
-    
-      * `id`
-      
-        * Integer.
-      
-        * Doesn't allow null values.
-      
-        * Set as primary key.
-      
-        * Uses auto increment.
-    
-      * `product_name`
-      
-        * String.
-      
-        * Doesn't allow null values.
-    
-      * `price`
-      
-        * Decimal.
-      
-        * Doesn't allow null values.
-      
-        * Validates that the value is a decimal.
-    
-      * `stock`
-      
-        * Integer.
-      
-        * Doesn't allow null values.
-      
-        * Set a default value of `10`.
-      
-        * Validates that the value is numeric.
-    
-      * `category_id`
-      
-        * Integer.
-      
-        * References the `Category` model's `id`.
+          * `id`
+            * Integer.
+            * Doesn't allow null values.
+            * Set as primary key.
+            * Uses auto increment.
+         * `product_name`
+            * String.
+            * Doesn't allow null values.
+         * `price`
+            * Decimal.
+            * Doesn't allow null values.
+            * Validates that the value is a decimal.
+          * `stock`
+            * Integer.
+            * Doesn't allow null values.
+            * Set a default value of `10`.
+            * Validates that the value is numeric.
+          * `category_id`
+            * Integer.
+            * References the `Category` model's `id`.
     */
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true;
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInt: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      //references the 'category' model's 'id'.
+      //allowo null?
+    }
   },
   {
     sequelize,
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+//added dev dependencies
+const mySql2 = require('mySql2');
+const dotenv = require('dotenv');
+
+// import sequelize connection---IMPORTED ACTIVITY 1
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// sync sequelize models to the database, then turn on the server---SYNCED SERVER _ ACITVITY 1
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log(`App listening on port http://localhost:${PORT}!`));
 });
